9412bc72b4863cff33e69e2cdd313345b1cd5e1d
John <johnlam2478@gmail.com>
2019-12-11 23:29:16 -0500

    asdfg

--- js/main.js
+++ js/main.js
@@ -1,15 +1,4 @@
 $(document).ready(function () {
 	console.log("Document Ready");
 
-	//Homepage circle progress
-	// $('.progress-circle-1').circleProgress({
-	// 	size: 100,
-	// 	value: $(this).data('progress-value'),
-	// 	fill: {
-	// 		color: '#9F2A24'
-	// 	}
-	// }).on('circle-animation-progress', function (event, progress, stepValue) {
-	// 	$(this).find('strong').text(stepValue.toFixed(2).substr(1));
-	// });
-
 });
